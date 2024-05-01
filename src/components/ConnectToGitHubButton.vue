fcf0019ad3043e38e772b9d4487494167ffedce9
tahirkodx <tahir@kodxsystem.com>
2024-05-01 11:08:41 +0500

    Firebase auth removed

--- src/components/ConnectToGitHubButton.vue
+++ src/components/ConnectToGitHubButton.vue
@@ -1,30 +1,10 @@
 <template>
     <div>
         
-            <button @click="handleLogin">Log In</button>
+            <button >Log In (Not built yet)</button>
      
     </div>
 </template>
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { myMSALObj } from '../plugins/apiConfig/msalConfig';
-import { useAuth } from '../plugins/apiConfig/useAuth';
-const { login, logout, handleRedirect } = useAuth();
-const handleLogin = async () => {
-await login();
-};
-const handleLogout = () =>{
-   logout();
-};
-const initialize = async () => {
-try {
-await myMSALObj.initialize() // Call the initialize function
-} catch (error) {
-console.log('Initialization error', error);
-}
-};
-onMounted (async () => {
-await initialize();
-await handleRedirect();
-});
+
 </script>
